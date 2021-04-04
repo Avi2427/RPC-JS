8b2d92259d07e81ef497113f5533ccd0bf014ecf
PULI <79397844+Avi2427@users.noreply.github.com>
2021-04-04 11:38:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var rpc = require("discord-rpc")
+const client = new rpc.Client({ transport: 'ipc' })
+client.on('ready', () => {
+client.request('SET_ACTIVITY', {
+pid: process.pid,
+activity : {
+static void UpdatePresence()
+{
+    DiscordRichPresence discordPresence;
+    memset(&discordPresence, 0, sizeof(discordPresence));
+    discordPresence.state = "Playing Solo";
+    discordPresence.details = "Competitive";
+    discordPresence.startTimestamp = 1507665886;
+    discordPresence.endTimestamp = 1507665886;
+    discordPresence.largeImageText = "Numbani";
+    discordPresence.smallImageText = "Rogue - Level 100";
+    discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
+    discordPresence.partySize = 1;
+    discordPresence.partyMax = 5;
+    discordPresence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= ";
+    Discord_UpdatePresence(&discordPresence);
+}
+},
+}
+})
+})
+client.login({ clientId : "824985881232146442" }).catch(console.error);
